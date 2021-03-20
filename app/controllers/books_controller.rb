cc3f0872a9c8e060a1ec60ba0ef8201020a4db1a
mikanfirst <mmkkmkk4869@gmail.com>
2021-03-20 09:43:49 +0000

    [add]booksページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,28 @@
 class BooksController < ApplicationController
+  def index
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to '/books'
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:name, :body)
+  end
 end
