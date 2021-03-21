2638493218940f56c580b567f611d67516aab435
mikanfirst <mmkkmkk4869@gmail.com>
2021-03-21 06:51:36 +0000

    [add]ユーザーページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  root 'users#show'
   get 'about' => 'homes#about'
   resources :books
+  resources :users, only: [:show, :edit]
 end
